60900dd75bed399f3258e62e7a2ca531b777fe72
whao89 <whao@princeton.edu>
2016-10-09 15:54:39 -0400

    improved error handling

--- R/NCBI_snp_par.R
+++ R/NCBI_snp_par.R
@@ -7,25 +7,46 @@
 #' @param hownice how nice? options are: c("very", "somewhat", "not")
 #' 
 
-NCBI_snp_query_par <- function(SNPs, blocksize=200, numcores=28, hownice="very"){
+NCBI_snp_query_par <- function(SNPs, blocksize=200, numcores=36, hownice="very"){
   hownice <- match.arg(hownice, c("very", "somewhat", "not"))
   if(hownice=="very"){
-    nice <- function(){Sys.sleep(runif(1, 3, 8))}
+    nice <- function(){Sys.sleep(runif(1, 1, 10))}
   } else if(hownice=="somewhat"){
-    nice <- function(){Sys.sleep(runif(1, 0.25, 3))}
+    nice <- function(){Sys.sleep(runif(1, 1, 5))}
   } else if(hownice=="not") {
-    nice <- function(){Sys.sleep(0.1)}
+    nice <- function(){Sys.sleep(runif(1, 0.01, 0.5))}
   }
   
   GROUPS <- rep(1:(length(SNPs)/blocksize+1), blocksize)
   GROUPS <- sort(GROUPS[1:length(SNPs)])
+  
+  query_dbSNP <- function(group){
+    nice()
+    ret <- NCBI_snp_query2(SNPs[GROUPS==group])
+    as.data.frame(ret$summary)
+  }
+  
   LDF <- mclapply(1:max(GROUPS),
-	    function(group){
-	      nice()
-	      ret <- NCBI_snp_query2(SNPs[GROUPS==group])
-	      nice()
-	      ret
-	    },
+	    query_dbSNP,
 	    mc.cores=numcores)
+  
+  # mclapply returns try-error for failed threads
+  
+  is_not_DF <- sapply(LDF, class) != "data.frame"
+  
+  while(sum(is_not_DF) != 0) {
+    print("some elements not data.frame, requerying")
+    
+    new_LDF <- mclapply( which(is_not_DF),
+		  query_dbSNP,
+		  mc.cores=numcores)
+    
+    for(s in 1:sum(is_not_DF)){
+      LDF[[ which(is_not_DF)[s] ]] <- new_LDF[[s]]
+    }
+    
+    is_not_DF <- sapply(LDF, class) != "data.frame"
+  }
+  
   do.call("rbind", LDF)
 }
